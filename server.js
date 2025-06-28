9efd5c1600eac3b02bea7e6ad8e69927e6c77fe2
StephanieEssis <stephanieessis@gmail.com>
2025-06-28 10:59:51 +0000

    fix:room

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 // Configuration CORS
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:3000'],
+  origin: true, // Autorise toutes les origines
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
